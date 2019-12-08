f67677998f12f9116d2449b6200b9da60869d26a
Jon Klingensmith <jonklingensmith@gmail.com>
2019-12-08 16:30:52 -0600

    Waffle pushed in testing.

--- TeamCode/src/main/java/Test_Auto.java
+++ TeamCode/src/main/java/Test_Auto.java
@@ -68,58 +68,10 @@ public class Test_Auto extends SkyStoneAutonomousMethods {
         robot.RightWaffle.setPosition(0); //grabs waffle
         robot.LeftWaffle.setPosition(1);
         sleep(400);
-        rotate(225,1); //rotates to align the waffle with building zone needs to be 240 to 250
-        strafe(60,1); //strafe waffle into build site
+        rotate(245,1); //rotates to align the waffle with building zone needs to be 240 to 250 was 225
+        strafe(75,1); //strafe waffle into build site
         telemetry.update();
 
-        //drive(16,  -1);     //Drive Back
-/*
-        switch (StoneRember) {
-            case 2: //SkyStone is on the right
-                strafe(100, -1);  //Sideways Under Bridge
-
-                robot.OpenServo.setPosition(0.36);  // Open Claw
-                sleep(150);     //way to big was 7 00     // Sleeping to drop block
-                strafe(169, 1);  // Going Back to grab another block
-                drive(23, 1);   // Going forward to grab block //was 16
-
-                grabBlock();
-
-                drive(18, -1);
-                armExt(2000, 1);
-                strafe(181, -1); //was 171
-                robot.OpenServo.setPosition(0.36);
-                break;
-            case 1: //SkyStone is on the left
-                strafe(120, -1);  //Sideways Under Bridge
-
-                robot.OpenServo.setPosition(0.36);  // Open Claw
-                sleep(400);     //way to big was 7 00     // Sleeping to drop block
-                strafe(190, 1);  // Going Back to grab another block
-                drive(18, 1);   // Going forward to grab block
-
-                grabBlock();
-
-                drive(18, -1);
-                armExt(2000, 1);
-                strafe(200, -1);
-            break;
-            case 0: //SkyStone is on the offscreen
-                strafe(145, -1);  //Sideways Under Bridge
-
-                robot.OpenServo.setPosition(0.36);  // Open Claw
-                sleep(400);     //way to big was 7 00     // Sleeping to drop block
-                strafe(40,1);
-                /*
-                strafe(240, 1);  // Going Back to grab another block
-                drive(16,    -1);   // Going forward to grab block
-
-                grabBlock();
-
-                drive(18, -1);
-                armExt(2000, 1);
-                strafe(220, -1); */
-           // break;
         }
     }
 
